d069636504cb7d730e2ffede456ba6cf8cbaed2d
Jineeth <jin123vijayan3@gmail.com>
2025-03-08 11:34:12 +0530

    fixing mongodb connection check-01

--- config/database.js
+++ config/database.js
@@ -1,38 +1,39 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
+console.log("MONGO_URI:", process.env.dbUrl);
 
-// const connectDB = async()=>{
-//     try {
-//         await mongoose.connect(process.env.dbUrl);
-//         console.log('Mongo Db Connected');
+const connectDB = async()=>{
+    try {
+        await mongoose.connect(process.env.dbUrl);
+        console.log('Mongo Db Connected');
         
-//     } catch (error) {
-//         console.log('Error Connecting MongoDB, ',error);
+    } catch (error) {
+        console.log('Error Connecting MongoDB, ',error);
         
-//     }
-// }
+    }
+}
 
-
-// export default connectDB ;
-
-
-
-let isConnected = false;
-
-const connectDB = async () => {
-  if (isConnected) return;
-  
-  try {
-    await mongoose.connect(process.env.dbUrl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    isConnected = true;
-    console.log("MongoDB Connected!");
-  } catch (error) {
-    console.error("MongoDB Connection Error:", error);
-  }
-};
 
 export default connectDB ;
+
+
+
+// let isConnected = false;
+
+// const connectDB = async () => {
+//   if (isConnected) return;
+  
+//   try {
+//     await mongoose.connect(process.env.dbUrl, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     isConnected = true;
+//     console.log("MongoDB Connected!");
+//   } catch (error) {
+//     console.error("MongoDB Connection Error:", error);
+//   }
+// };
+
+// export default connectDB ;
